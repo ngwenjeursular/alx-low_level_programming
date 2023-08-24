65d8e941149b243602f5dbbf51d50e6882c4a1bb
jackie <ngwenjejackline@gmail.com>
2023-08-24 15:08:18 -0700

    Tortoise

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
-void prints_before(void) __attribute__ ((constructor));
+void first(void) __attribute__ ((constructor));
 
 /**
  * first - prints a sentence before the main
  * function is executed
  */
-void prints_before(void)
+
+void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
